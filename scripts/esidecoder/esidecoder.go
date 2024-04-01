3c864c392e7aa77fc36cb7867d325882bf3f2c3e
Scott Laird <scott@sigkill.org>
2024-04-01 10:14:14 -0700

    docs: Update ESI decoder to decode DC settings

--- scripts/esidecoder/esidecoder.go
+++ scripts/esidecoder/esidecoder.go
@@ -4,11 +4,11 @@ import (
 	"encoding/xml"
 	"flag"
 	"fmt"
-	"strconv"
 	"golang.org/x/net/html/charset"
 	"gopkg.in/yaml.v3"
 	"os"
 	"path/filepath"
+	"strconv"
 	"strings"
 )
 
@@ -47,20 +47,28 @@ type ESIDevice struct {
 		CheckRevisionNo string `xml:"CheckRevisionNo,attr"`
 	} `xml:"Type" yaml:"-"`
 
-	Names     []ESIName      `xml:"Name" yaml:",omitempty"`
-	URLs      []ESIDeviceURL `xml:"URL" yaml:",omitempty"`
-	Info      ESIDeviceInfo  `xml:"Info" yaml:",omitempty"`
-	GroupType string         `xml:"GroupType" yaml:",omitempty"` // maps to ESIGroup.Type
-	Fmmu      []string       `xml:"Fmmu" yaml:",omitempty,flow"` // "Inputs"/"Outputs"
+	Names []ESIName      `xml:"Name" yaml:",omitempty"`
+	URLs  []ESIDeviceURL `xml:"URL" yaml:",omitempty"`
+	//Info      ESIDeviceInfo  `xml:"Info" yaml:",omitempty"`
+	GroupType string `xml:"GroupType" yaml:",omitempty"` // maps to ESIGroup.Type
+	//Fmmu      []string       `xml:"Fmmu" yaml:",omitempty,flow"` // "Inputs"/"Outputs"
 
-	ShortType   string `yaml:"Type"`
-	ProductCode string `yaml:"ProductCode"`
-	RevisionNo  string `yaml:"RevisionNo"`
-	EnglishURL  string `yaml:"URL"`
-	EnglishName string `yaml:"Name"`
-	GroupName   string `yaml:"DeviceGroup"`
-	Vendor      string `yaml:"Vendor"`
-	VendorID    string `yaml:"VendorID"`
+	ShortType      string       `yaml:"Type"`
+	ProductCode    string       `yaml:"ProductCode"`
+	RevisionNo     string       `yaml:"RevisionNo"`
+	EnglishURL     string       `yaml:"URL"`
+	EnglishName    string       `yaml:"Name"`
+	GroupName      string       `yaml:"DeviceGroup"`
+	VendorName     string       `yaml:"Vendor"`
+	VendorID       string       `yaml:"VendorID"`
+	AssignActivate string       `yaml:"AssignActivate"`
+	SyncCycle0     string       `yaml:"SyncCycle0,omitempty"`
+	SyncCycle1     string       `yaml:"SyncCycle1,omitempty"`
+	SyncFactor0    string       `yaml:"SyncFactor0,omitempty"`
+	SyncFactor1    string       `yaml:"SyncFactor1,omitempty"`
+	SyncShift0     string       `yaml:"SyncShift0,omitempty"`
+	SyncShift1     string       `yaml:"SyncShift1,omitempty"`
+	OpModes        []*ESIOpMode `xml:"Dc>OpMode" yaml:",omitempty"`
 }
 
 type ESIGroup struct {
@@ -70,11 +78,31 @@ type ESIGroup struct {
 }
 
 type ESIData struct {
-	EtherCATInfo xml.Name     `xml:"EtherCATInfo"`
-	VendorID     string       `xml:"Vendor>Id"`
-	VendorName   string       `xml:"Vendor>Name"`
+	EtherCATInfo xml.Name  `xml:"EtherCATInfo"`
+	VendorID     string    `xml:"Vendor>Id"`
+	Vendors      []ESIName `xml:"Vendor>Name"`
+	VendorName   string
 	Groups       []*ESIGroup  `xml:"Descriptions>Groups>Group"`
 	Devices      []*ESIDevice `xml:"Descriptions>Devices>Device"`
+}
+
+type ESIOpMode struct {
+	//	OpMode         xml.Name `xml:"OpMode"`
+	Name           string `xml:"Name"`
+	Desc           string `xml:"Desc"`
+	AssignActivate string `xml:"AssignActivate"`
+	CycleTimeSync0 struct {
+		N      xml.Name `xml:"CycleTimeSync0"`
+		Value  string   `xml:",chardata"`
+		Factor string   `xml:"Factor,attr"`
+	}
+	CycleTimeSync1 struct {
+		N      xml.Name `xml:"CycleTimeSync1"`
+		Value  string   `xml:",chardata"`
+		Factor string   `xml:"Factor,attr"`
+	}
+	ShiftTimeSync0 string `xml:"ShiftTimeSync0"`
+	ShiftTimeSync1 string `xml:"ShiftTimeSync1"`
 }
 
 func parseESIFile(filename string) ([]*ESIDevice, error) {
@@ -105,6 +133,16 @@ func parseESIFile(filename string) ([]*ESIDevice, error) {
 		}
 	}
 
+	vendor := "Unknown"
+	if len(esiData.Vendors) > 0 {
+		vendor = esiData.Vendors[0].Name
+		for _, v := range esiData.Vendors {
+			if v.LanguageID == "1033" { // English
+				vendor = v.Name
+			}
+		}
+	}
+
 	// We have a fair amount of post-processing to do per Device.
 	//
 	// - We want to get the English name and URLs
@@ -113,12 +151,12 @@ func parseESIFile(filename string) ([]*ESIDevice, error) {
 	// - We want to reformat the product code and revision to be 0xXXX-format hex numbers instead of #xXXX
 	// - We want to strip extra data (non-English names and URLs, group data, etc
 	for _, d := range esiData.Devices {
-		d.Vendor = esiData.VendorName
-		d.VendorID = fixHexFormat(esiData.VendorID)
+		d.VendorID = fixHexFormat(esiData.VendorID, 8)
+		d.VendorName = vendor
 
 		for _, n := range d.Names {
 			if n.LanguageID == "1033" { // English
-				d.EnglishName = n.Name
+				d.EnglishName = strings.TrimSpace(n.Name)
 			}
 		}
 		for _, u := range d.URLs {
@@ -126,8 +164,26 @@ func parseESIFile(filename string) ([]*ESIDevice, error) {
 				d.EnglishURL = u.URL
 			}
 		}
-		d.Type.ProductCode = fixHexFormat(d.Type.ProductCode)
-		d.Type.RevisionNo = fixHexFormat(d.Type.RevisionNo)
+		for _, o := range d.OpModes {
+			a := fixHexFormat(o.AssignActivate, 3)
+			aa, _ := strconv.ParseInt(a, 0, 64)
+
+			if aa >= 0x300 {
+				d.AssignActivate = a
+				d.SyncShift0 = o.ShiftTimeSync0
+				d.SyncShift1 = o.ShiftTimeSync1
+				d.SyncCycle0 = o.CycleTimeSync0.Value
+				d.SyncFactor0 = o.CycleTimeSync0.Factor
+				d.SyncCycle1 = o.CycleTimeSync1.Value
+				d.SyncFactor1 = o.CycleTimeSync1.Factor
+				break
+			}
+		}
+
+		d.OpModes = nil
+
+		d.Type.ProductCode = fixHexFormat(d.Type.ProductCode, 8)
+		d.Type.RevisionNo = fixHexFormat(d.Type.RevisionNo, 8)
 		d.GroupName = groupMap[d.GroupType]
 		d.ShortType = d.Type.Type
 		d.ProductCode = d.Type.ProductCode
@@ -141,7 +197,7 @@ func parseESIFile(filename string) ([]*ESIDevice, error) {
 	return devices, nil
 }
 
-func fixHexFormat(in string) string {
+func fixHexFormat(in string, nibbles int) string {
 	// No need to "fix" empty strings
 	if len(in) == 0 {
 		return in
@@ -155,7 +211,7 @@ func fixHexFormat(in string) string {
 		if err != nil {
 			panic(err)
 		}
-		return fmt.Sprintf("0x%08x", v)
+		return fmt.Sprintf("0x%0*x", nibbles, v)
 	}
 
 	// Okay, it's probably just an integer.  Beckhoff likes that.  Convert to hex.
@@ -164,7 +220,7 @@ func fixHexFormat(in string) string {
 		panic(err)
 	}
 
-	return fmt.Sprintf("0x%08x", i)
+	return fmt.Sprintf("0x%0*x", nibbles, i)
 }
 
 func main() {
